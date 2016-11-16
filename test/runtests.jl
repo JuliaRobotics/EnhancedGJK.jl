a2db0020218b7b4a7e074b902a67fa729011cbc8
Robin Deits <robin.deits@gmail.com>
2016-11-16 13:12:05 -0500

    test penetration sign, but not value

--- test/runtests.jl
+++ test/runtests.jl
@@ -36,13 +36,21 @@ end
 @testset "mesh to mesh" begin
     mesh = load("meshes/r_foot_chull.obj")
     dx = 1.0
+    foot_length = 0.172786 + 0.090933
 
     cache = EnhancedGJK.CollisionCache(mesh, mesh)
     result = EnhancedGJK.gjk!(cache, IdentityTransformation(), Translation(SVector(dx, 0, 0)))
-    @test isapprox(result.signed_distance, dx - (0.172786 + 0.090933), atol=1e-3)
+    @test isapprox(result.signed_distance, dx - foot_length, atol=1e-3)
 
+    cache = EnhancedGJK.CollisionCache(mesh, mesh)
     result = EnhancedGJK.gjk!(cache, Translation(SVector(dx, 0, 0)), IdentityTransformation())
-    @test isapprox(result.signed_distance, dx - (0.172786 + 0.090933), atol=1e-3)
+    @test isapprox(result.signed_distance, dx - foot_length, atol=1e-3)
+
+    cache = EnhancedGJK.CollisionCache(mesh, mesh)
+    expected_penetration = 0.01
+    result = EnhancedGJK.gjk!(cache, IdentityTransformation(), Translation(foot_length - expected_penetration, 0, 0))
+    # TODO: penetration distance is inconsistent and inaccurate
+    @test result.signed_distance < 0
 end
 
 @testset "benchmarks" begin
