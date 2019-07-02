974e9985dc62b30af7771ceef3f17afab09755a8
Twan Koolen <koolen.twan@gmail.com>
2019-07-02 11:02:07 -0400

    Inline code generation into generated function body for easier memory allocation debugging. Define num_johnson_subsets and johnson_subsets before generated function as the documentation tells us.

--- src/johnson_distance.jl
+++ src/johnson_distance.jl
@@ -1,22 +1,3 @@
-"""
-    weights = projection_weights(simplex)
-
-This function implements Johnson's distance subalgorithm, as described in
-E. G. Gilbert, D. W. Johnson, and S. S. Keerthi, “A fast procedure for
-computing the distance between complex objects in three-dimensional space,”
-1988. Given a simplex (a length N+1 vector of points of dimension N), it
-returns weights such that dot(weights, simplex) yields the point in the convex
-hull of the simplex which is closest to the origin.
-
-This is the critical loop of the GJK algorithm, so it has been heavily optimized
-to precompute, inline, and unroll as much of the algorithm as possible. For a
-more readable (and much slower) implementation, see
-projection_weights_reference()
-"""
-@generated function projection_weights(simplex::SVector{M, SVector{N, T}}) where {M,N,T}
-    projection_weights_impl(simplex)
-end
-
 num_johnson_subsets(simplex_length::Integer) = 2^simplex_length - 1
 
 """
@@ -37,8 +18,26 @@ function johnson_subsets(simplex_length::Integer)
     subsets
 end
 
-function initial_deltas_impl(simplex_length, T)
-    num_subsets = num_johnson_subsets(simplex_length)
+"""
+    weights = projection_weights(simplex)
+
+This function implements Johnson's distance subalgorithm, as described in
+E. G. Gilbert, D. W. Johnson, and S. S. Keerthi, “A fast procedure for
+computing the distance between complex objects in three-dimensional space,”
+1988. Given a simplex (a length N+1 vector of points of dimension N), it
+returns weights such that dot(weights, simplex) yields the point in the convex
+hull of the simplex which is closest to the origin.
+
+This is the critical loop of the GJK algorithm, so it has been heavily optimized
+to precompute, inline, and unroll as much of the algorithm as possible. For a
+more readable (and much slower) implementation, see
+projection_weights_reference()
+"""
+@generated function projection_weights(simplex::SVector{M, SVector{N, T}}) where {M,N,T}
+    simplex_length = M
+    num_subsets = num_johnson_subsets(M)
+    subsets = johnson_subsets(M)
+    complements = .!subsets
 
     expr = quote
         deltas = zero(SVector{$num_subsets, SVector{$simplex_length, $T}})
@@ -53,24 +52,6 @@ function initial_deltas_impl(simplex_length, T)
         push!(expr.args, quote
             deltas = setindex(deltas, $(arg), $(2^(i - 1)))
         end)
-    end
-    expr
-end
-
-"""
-This is the function which actually computes the weight values.
-It returns an *expression* to compute that result, with all loops
-unrolled and all indices pre-computed based on the size and dimension
-of the simplex. For a more readable version of the same function, check
-out projection_weights_reference().
-"""
-function projection_weights_impl(::Type{SVector{M, SVector{N, T}}}) where {M,N,T}
-    num_subsets = num_johnson_subsets(M)
-    subsets = johnson_subsets(M)
-    complements = .!subsets
-
-    expr = quote
-        deltas = $(initial_deltas_impl(M, T))
     end
 
     for s in 1:(num_subsets - 1)
